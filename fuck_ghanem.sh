da73bff0d0a5069d56f057b02179843d7cfe7292
Owen Howell <ohowell74@gmail.com>
2025-05-13 02:53:33 +0000

    fuck ghanem bash added

---
+++ fuck_ghanem.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+for task in airl rgcl gail; do
+    for seed in {123..134}; do
+        echo "Running task --$task with seed $seed"
+        python main.py \
+            --seed="$seed" \
+            --horizon=50 \
+            --N_steps=200 \
+            --gym_env=Hopper \
+            --lr=1e-4 \
+            --num_traj=500 \
+            --reward_fn_updates=15 \
+            --lambda_=0.01 \
+            --rirl_iterations=100 \
+            --Q=1e-4 \
+            --P=1e-2 \
+            --hidden_dim=16 \
+            --"$task" \
+            >> "output_seed_${seed}_${task}.log" 2>&1
+    done
+done
+
